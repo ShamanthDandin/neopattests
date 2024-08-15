a23683936dd499bbc9b5d9ab8fac595b4f64bc79
Sampi <97970169+ShamanthDandin@users.noreply.github.com>
2024-08-15 23:18:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ class Solution{
         int m = sc.nextInt();
         int n = sc.nextInt();
         int[][] arr = new int[m][n];
-        for(int i = 0;j<n;j++){
+        for(int i = 0;i<n;i++){
             for(int j = 0;j<n;j++){
                 arr[i][j] = sc.nextInt();
             }
@@ -80,7 +80,7 @@ rot[(i+m)%n]=part[i];
 } 
 
 for(int i=0;i<n;i++) {
-    cout<<rot[i]<<<" ";
+    cout<<rot[i]<<" ";
  } 
 
 cout<<'\n';
