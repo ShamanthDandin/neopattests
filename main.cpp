d554020b54b1c4f26ba5d79867429a20408d3948
Sampi <97970169+ShamanthDandin@users.noreply.github.com>
2024-09-05 23:03:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,171 +1,78 @@
-//ABDUL
-#include <iostream>
-#include <vector>
-using namespace std;
+//matrix output question
+def create(v,e,edges):
+    am=[[0 for _ in range(v)] for _ in range(v)]
+    for edge in edges:
+        u,w=edge
+        am[u-1][w-1]=1
+        am[w-1][u-1]=1
+    return am
+def Print(matrix):
+    for row in matrix:
+        print(' '.join(map(str,row)))
+v=int(input())
+e=int(input())
+edges=[tuple(map(int,input.split())) for _ in range(e)]
+am=create(v,e,edges)
+Print(am)
 
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    int height;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr), height(1) {}
-};
-
-int getHeight(TreeNode* node) {
-    return node ? node->height : 0;
-}
-
-int getBalance(TreeNode* node) {
-    return node ? getHeight(node->left) - getHeight(node->right) : 0;
-}
-
-TreeNode* rotateRight(TreeNode* y) {
-    TreeNode* x = y->left;
-    TreeNode* T2 = x->right;
-    x->right = y;
-    y->left = T2;
-    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
-    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
-    return x;
-}
-
-TreeNode* rotateLeft(TreeNode* x) {
-    TreeNode* y = x->right;
-    TreeNode* T2 = y->left;
-    y->left = x;
-    x->right = T2;
-    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
-    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
-    return y;
-}
-
-TreeNode* insert(TreeNode* node, int key) {
-    if (!node) return new TreeNode(key);
-    if (key < node->val)
-        node->left = insert(node->left, key);
-    else if (key > node->val)
-        node->right = insert(node->right, key);
-    else
-        return node;
-
-    node->height = 1 + max(getHeight(node->left), getHeight(node->right));
-    int balance = getBalance(node);
-
-    if (balance > 1 && key < node->left->val)
-        return rotateRight(node);
-
- 
-    if (balance < -1 && key > node->right->val)
-        return rotateLeft(node);
- 
-    if (balance > 1 && key > node->left->val) {
-        node->left = rotateLeft(node->left);
-        return rotateRight(node);
-    }
-
-    if (balance < -1 && key < node->right->val) {
-        node->right = rotateRight(node->right);
-        return rotateLeft(node);
-    }
-
-    return node;
-}
-
-void preOrder(TreeNode* root) {
-    if (!root) return;
-    cout << root->val << " ";
-    preOrder(root->left);
-    preOrder(root->right);
-}
-
-int main() {
-    int n;
-    cin >> n;
-
-    vector<int> plants(n);
-    for (int i = 0; i < n; ++i)
-        cin >> plants[i];
-
-    TreeNode* root = nullptr;
-    for (int code : plants)
-        root = insert(root, code);
-
-    preOrder(root);
-    cout << endl;
-
-    return 0;
-}
+//Visited Node 
+from collections import deque
+def bfs(graph,start,v):
+    visited=[False]*v
+    queue=deque([start])
+    visited[start]=True
+    result=[]
+    while queue:
+        node=queue.popleft()
+        result.append(f"Visited Node:{node}")
+        for n in graph[node]:
+            if not visited[n]:
+                queue.append(n)
+                visited[n]=True
+    return result;
+def main():
+    v,e=map(int,input().split())
+    graph=[[] for _ in range(v)]
+    for _ in range(e):
+        p1,p2=map(int,input().split())
+        graph[p1].append(p2)
+        graph[p2].append(p1)
+    start_node=int(input())
+    bfs_res=bfs(graph,start_node,v)
+    for node in bfs_res:
+        print(node)
+if _name=="main_":
+    main()
 
 
-// Koyal
-import java.util.*;
+//vertex
+def construct_graph(V, edges):
+    adjacency_list = [[] for _ in range(V)]
+    for x, y in edges:
+        adjacency_list[x].append(y)
+        adjacency_list[y].append(x)
+    for neighbors in adjacency_list:
+        neighbors.sort()
+    return adjacency_list
 
-class Solution{
-     public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      int n = sc.nextInt();
-      if(n > 0){
-         for(int i = 0; i < n-1;i++){
-            System.out.print(sc.nextInt()+" ");
-         }
-      } else {
-System.out.println("Tree is empty");
-      }
-     }
-}
+def Print(adjacency_list):
+    for vertex, neighbors in enumerate(adjacency_list):
+        if neighbors:
+            neighbors_str = '->'.join(map(str, neighbors))
+        else:
+            neighbors_str = ''
+        print(f"Vertex {vertex}: {neighbors_str}")
 
-//Thiru
-#include<bits/stdc++.h>
-using namespace std;
-Node* insert(Node* root,char val);
-void inOrder(Node* root);
+def main():
+    V = int(input())
+    E = int(input())
+    edges = []
+    for _ in range(E):
+        x, y = map(int, input().split())
+        edges.append((x, y))
+    
+    adjacency_list = construct_graph(V, edges)
+    Print(adjacency_list)
 
-struct Node{
-char data;
-Node* left;
-Node* right;
-Node(char val)
-{
-   data = val;
-   left = nullptr;
-   right = nullptr;
-};
-Node* insert(Node* root,char val){
- if(root == nullptr)
-{
-return new Node(val);
-}
-if(val < root->data){
-root -> left = insert(root->left,val);
-}
-else if(val >root->data){
-root -> right = insert(root -> right,val)
- }
-return root;
-}
-
-void inOrder(Node* root){
- if(root == nullptr) return;
-inOrder(root->left);
-cout<<root->data<<" ";
-inOrder(root->right);
-}
-
-int main(){
- int n;
-cin >> n;
-cin.ignore();
-string line;
-getline(cin,line);
-istringstream iss(line);
-
-Node* root = nullptr;
-char c;
-while(iss >> c)
-{
-root = insert(root,c);
-}
-inOrder(root);
-cout<<'\n';
-return 0;
-}
+if _name_ == "_main_":
+    main()
